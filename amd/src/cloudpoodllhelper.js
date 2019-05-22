dfb0f3b934a6224a1a189d87eee2788c21bdeaa5
justinhunt <bitwalkerjapan@gmail.com>
2019-05-22 23:36:33 +0900

    tweaks for instances

--- amd/src/cloudpoodllhelper.js
+++ amd/src/cloudpoodllhelper.js
@@ -13,8 +13,47 @@ define(["jquery", "core/log", "qtype_cloudpoodll/cloudpoodllloader"], function (
             config.transcriber = opts["transcriber"];
             config.uploadstate= false;
 
-            config = this.register_controls(config);
-            this.setup_recorder(config);
+            //register controls
+            var name = CSS.escape(config.inputname);
+            config.controls = {
+                mediaurl: $("input[name=" + name + "mediaurl]"),
+                transcript: $("input[name=" + name + "transcript]"),
+                answer: $("input[name=" + name + "]"),
+            };
+
+            //setup recorder
+            var gspeech = "";
+            var recorder_callback = function (evt) {
+                switch (evt.type) {
+                    case "recording":
+                        if (evt.action === "started") {
+                            gspeech = "";
+                            // that.controls.updatecontrol.val();
+                        }
+                        break;
+                    case "speech":
+                        gspeech += "  " + evt.capturedspeech;
+                        config.controls.transcript.val(gspeech);
+                        config.controls.answer.val(gspeech);
+                        break;
+                    case "awaitingprocessing":
+                        if (config.uploadstate != "posted") {
+                            config.controls.mediaurl.val(evt.mediaurl);
+                            config.controls.answer.val(evt.mediaurl);
+                        }
+                        config.uploadstate = "posted";
+                        break;
+                    case "error":
+                        alert("PROBLEM: " + evt.message);
+                        break;
+                }
+            };
+            var cp = cloudpoodll.clone();
+            cp.init(config.dom_id, recorder_callback);
+
+            //defunct
+            //config = this.register_controls(config);
+            //this.setup_recorder(config);
         },
 
         setup_recorder: function (config) {
