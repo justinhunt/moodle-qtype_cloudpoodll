746fa10c7c7a1b1e6579b7a385f8fe6bdbc6626d
justinhunt <bitwalkerjapan@gmail.com>
2023-04-16 05:07:06 +0000

    version bump

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023022600;
+$plugin->version = 2023041500;
 $plugin->requires = 2016052300;//moodle 3.1.0
 $plugin->component = 'qtype_cloudpoodll';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.0.15 (Build 2023022600)';
+$plugin->release = '1.0.16 (Build 2023041500)';
 
